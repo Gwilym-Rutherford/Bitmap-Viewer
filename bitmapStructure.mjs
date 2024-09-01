536634d00b858e9f6613400fd8b9c2bf3ad8ade5
Gwilym-Rutherford <58346802+Gwilym-Rutherford@users.noreply.github.com>
2024-09-01 22:30:19 +0100

    store important information about image

---
+++ bitmapStructure.mjs
@@ -0,0 +1,9 @@
+export const BitmapStructure = {
+    Signature: {offset: 0x00, size: 2},
+    FileSize: {offset: 0x02, size: 4},
+    DataOffset: {offset: 0x0a, size: 4},
+    ImageSize: {offset: 0x22, size: 4},
+    Width: {offset: 0x12, size: 4},
+    Height: {offset: 0x16, size: 4}
+};
+
--- main.mjs
+++ main.mjs
@@ -1,4 +1,5 @@
 import fs from "fs";
+import { BitmapStructure } from "./BitmapStructure.mjs";
 
 fs.readFile("./checkers_test.bmp", "hex", (err, data)=>{
     if(err){
@@ -6,18 +7,20 @@ fs.readFile("./checkers_test.bmp", "hex", (err, data)=>{
     }
     
     let rawHexData = data;
-    let formattedHexData = new Array(new Array(16))
-    
-    let numberOfRows = (rawHexData.length/2)/16;
-    for(let i = 0; i < numberOfRows; i++){
-        
-        let rowData = getNRow(rawHexData, i);
-        formattedHexData[i] = rowData.match(/.{1,2}/g);
-    };
+
+    console.log("!!!!!This is in little endian!!!!!")
+
+    const formattedHexData = new Map();
+    let keys = Object.keys(BitmapStructure);
+    keys.forEach((attribute)=>{
+        let offset = BitmapStructure[attribute].offset * 2;
+        let size = BitmapStructure[attribute].size * 2;
+        let data = rawHexData.substring(offset, offset + size);
+
+        formattedHexData.set(attribute, data);
+        console.log(attribute + ": " + data);
+    });
+
+    //console.log("Raster Data: " + rawHexData.substring(formattedHexData.get("DataOffset") * 2, rawHexData.length));
 })
 
-function getNRow(rawData, rowNumber){
-    const ROW_OFFSET = 32
-    let address = rowNumber * ROW_OFFSET;
-    return rawData.substring(address, address + ROW_OFFSET); 
-}
